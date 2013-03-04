f9e8c9315f91e661cdecb61a48c7a452ec98f38e
Jeff Wear <jeff@inkling.com>
2013-03-04 10:19:48 -0800

    Initial Commit

---
+++ BuzzBox/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  BuzzBox
+//
+//  Created by Jeffrey Wear on 3/4/13.
+//  Copyright (c) 2013 Jeffrey Wear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BuzzBox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BuzzBox
+//
+//  Created by Jeffrey Wear on 3/4/13.
+//  Copyright (c) 2013 Jeffrey Wear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BBAppDelegate class]));
+    }
+}
